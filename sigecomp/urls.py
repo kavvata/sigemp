b826fcebfd407633893213b2699d0ede90a52365
kavvata <gabriel.kawata@gmail.com>
2024-11-10 14:27:35 -0300

    Related #3: Configurado rota para hot-reload do tailwind

--- sigecomp/urls.py
+++ sigecomp/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("ativos/", include("ativos.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
