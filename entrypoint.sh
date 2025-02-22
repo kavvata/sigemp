0def01ab9d55b098436e139b6e4f83290f43f2bb
kavvata <gabriel.kawata@gmail.com>
2025-02-22 10:48:50 -0300

    Corrigido domain name do servidor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
 while true; do
-    glpi-agent --server http://localhost:8000/ativos/inventory/
+    glpi-agent --server http://host.docker.internal:8000/ativos/inventory/
     sleep 300
 done
