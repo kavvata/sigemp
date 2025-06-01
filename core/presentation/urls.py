95cbf3e942fd3b65d415eb5239426d5f008ee33d
kavvata <gabriel.kawata@gmail.com>
2025-06-01 20:16:32 -0300

    feat(core): adicionado rota para logout

--- core/presentation/urls.py
+++ core/presentation/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "core"
 urlpatterns = [
     path("", views.home_view, name="home"),
-    path("login/", views.login_view, name="login"),
+    path("accounts/login/", views.login_view, name="login"),
+    path("accounts/logout/", views.logout_view, name="logout"),
 ]
--- core/presentation/views.py
+++ core/presentation/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.http.request import HttpRequest
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_not_required
 from django.urls import reverse
 
 from core.presentation.forms import LoginForm
@@ -11,6 +12,7 @@ from core.usecases import login_usecase
 
 
 # Create your views here.
+@login_not_required
 def login_view(request: HttpRequest):
     if request.method == "POST":
         form = LoginForm(request.POST)
@@ -36,3 +38,8 @@ def login_view(request: HttpRequest):
 
 def home_view(request: HttpRequest):
     return render(request, "core/home.html")
+
+
+def logout_view(request: HttpRequest):
+    logout(request)
+    return redirect(reverse("core:login"))
