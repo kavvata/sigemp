e29cab54706d427c7a68df3e289495868a289687
kavvata <gabriel.kawata@gmail.com>
2025-06-04 11:27:19 -0300

    feat(patrimonio): incluido metodo para buscar por id em TipoBemRepository

--- patrimonio/repositories/contracts.py
+++ patrimonio/repositories/contracts.py
@@ -3,17 +3,21 @@ from abc import ABC, abstractmethod
 
 class TipoBemRepository(ABC):
     @abstractmethod
-    def listar_tipos_bem(self) -> any:
+    def listar_tipos_bem(self):
         pass
 
     @abstractmethod
-    def cadastrar_tipo_bem(self, descricao: str) -> any:
+    def buscar_tipo_bem_por_id(self, id: int):
         pass
 
     @abstractmethod
-    def editar_tipo_bem(self, id: int, descricao: str) -> any:
+    def cadastrar_tipo_bem(self, descricao: str):
         pass
 
     @abstractmethod
-    def remover_tipo_bem(self, id: int, descricao: str) -> any:
+    def editar_tipo_bem(self, id: int, descricao: str):
+        pass
+
+    @abstractmethod
+    def remover_tipo_bem(self, id: int):
         pass
--- patrimonio/repositories/django.py
+++ patrimonio/repositories/django.py
@@ -5,17 +5,32 @@ from patrimonio.repositories.contracts import TipoBemRepository
 
 class DjTipoBemRepository(TipoBemRepository):
     @override
-    def cadastrar_tipo_bem(self, descricao: str) -> any:
-        return TipoBem.objects.create(descricao=descricao)
+    def listar_tipos_bem(self):
+        return TipoBem.objects.filter(ativo=True)
 
     @override
-    def editar_tipo_bem(self, id: int, descricao: str) -> any:
+    def buscar_tipo_bem_por_id(self, id: int):
         tipo_bem = None
 
         try:
             tipo_bem = TipoBem.objects.get(pk=id, ativo=True)
-        except TipoBem.DoesNotExist:
-            raise Exception(f"Tipo de bem com id {id} não encontrado.")
+        except TipoBem.DoesNotExist as e:
+            e.add_note(f"Tipo de bem com id {id} não encontrado.")
+            raise e
+
+    @override
+    def cadastrar_tipo_bem(self, descricao: str) -> TipoBem:
+        return TipoBem.objects.create(descricao=descricao)
+
+    @override
+    def editar_tipo_bem(self, id: int, descricao: str) -> TipoBem:
+        tipo_bem = None
+
+        try:
+            tipo_bem = TipoBem.objects.get(pk=id, ativo=True)
+        except TipoBem.DoesNotExist as e:
+            e.add_note(f"Tipo de bem com id {id} não encontrado.")
+            raise e
 
         try:
             TipoBem.objects.get(descricao=descricao, ativo=True)
@@ -30,12 +45,14 @@ class DjTipoBemRepository(TipoBemRepository):
         return tipo_bem
 
     @override
-    def remover_tipo_bem(self, id: int, descricao: str) -> any:
+    def remover_tipo_bem(self, id: int, descricao: str) -> TipoBem:
         tipo_bem = None
+
         try:
             tipo_bem = TipoBem.objects.get(pk=id, ativo=True)
-        except TipoBem.DoesNotExist:
-            raise Exception(f"Tipo de bem com id {id} não encontrado.")
+        except TipoBem.DoesNotExist as e:
+            e.add_note(f"Tipo de bem com id {id} não encontrado.")
+            raise e
 
         tipo_bem.ativo = False
         tipo_bem.save()
