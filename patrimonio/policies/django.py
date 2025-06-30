8fb640feb460517bf41631ee06f919a9dc73e6fe
kavvata <gabriel.kawata@gmail.com>
2025-06-30 00:08:49 -0300

    feat(patrimonio): implementado verificação de permissão em policies de tipos de bem

--- patrimonio/policies/django.py
+++ patrimonio/policies/django.py
@@ -10,20 +10,32 @@ class DjangoTipoBemPolicy(TipoBemPolicy):
 
     @override
     def pode_listar(self) -> bool:
-        return self.user.is_authenticated
+        return self.user.is_superuser or self.user.has_perm("patrimonio:view_tipobem")
 
     @override
     def pode_criar(self) -> bool:
-        return self.user.is_authenticated
+        return self.user.is_superuser or self.user.has_perm("patrimonio:add_tipobem")
 
     @override
     def pode_editar(self, tipo_bem: TipoBem) -> bool:
-        return self.user.is_authenticated and tipo_bem.removido_em is None
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("patrimonio:change_tipobem")
+            and tipo_bem.removido_em is None
+        )
 
     @override
     def pode_remover(self, tipo_bem) -> bool:
-        return self.user.is_authenticated and tipo_bem.removido_em is None
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("patrimonio:delete_tipobem")
+            and tipo_bem.removido_em is None
+        )
 
     @override
     def pode_visualizar(self, tipo_bem) -> bool:
-        return self.user.is_authenticated and tipo_bem.removido_em is None
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("patrimonio:view_tipobem")
+            and tipo_bem.removido_em is None
+        )
