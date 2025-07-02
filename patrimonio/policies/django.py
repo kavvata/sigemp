13da41567a2ec8d97b9537e566a10853f7a4221b
kavvata <gabriel.kawata@gmail.com>
2025-07-02 00:07:54 -0300

    feat(patrimonio): implementado policies para django de estados de conservação

--- patrimonio/policies/django.py
+++ patrimonio/policies/django.py
@@ -1,6 +1,6 @@
 from typing import override
-from patrimonio.models import TipoBem
-from patrimonio.policies.contracts import TipoBemPolicy
+from patrimonio.models import TipoBem, EstadoConservacao
+from patrimonio.policies.contracts import EstadoConservacaoPolicy, TipoBemPolicy
 from django.contrib.auth.models import User
 
 
@@ -38,4 +38,45 @@ class DjangoTipoBemPolicy(TipoBemPolicy):
             self.user.is_superuser
             or self.user.has_perm("patrimonio:view_tipobem")
             and tipo_bem.removido_em is None
+        )
+
+
+class DjangoEstadoConservacaoPolicy(EstadoConservacaoPolicy):
+    def __init__(self, user: User) -> None:
+        super().__init__(user)
+
+    @override
+    def pode_listar(self) -> bool:
+        return self.user.is_superuser or self.user.has_perm(
+            "patrimonio:view_estadoconservacao"
+        )
+
+    @override
+    def pode_criar(self) -> bool:
+        return self.user.is_superuser or self.user.has_perm(
+            "patrimonio:add_estadoconservacao"
+        )
+
+    @override
+    def pode_editar(self, estado_conservacao) -> bool:
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("patrimonio:change_estadoconservacao")
+            and estado_conservacao.removido_em is None
+        )
+
+    @override
+    def pode_remover(self, estado_conservacao) -> bool:
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("patrimonio:delete_estadoconservacao")
+            and estado_conservacao.removido_em is None
+        )
+
+    @override
+    def pode_visualizar(self, estado_conservacao) -> bool:
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("patrimonio:view_estadoconservacao")
+            and estado_conservacao.removido_em is None
         )
