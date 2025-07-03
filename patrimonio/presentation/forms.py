d9fb6a1ec91bb6551b4d602ec4ce877e92d37355
kavvata <gabriel.kawata@gmail.com>
2025-07-03 11:32:23 -0300

    feat(patrimonio): criado form para estados de conservacao

--- patrimonio/presentation/forms.py
+++ patrimonio/presentation/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from patrimonio.models import TipoBem
+from patrimonio.models import EstadoConservacao, TipoBem
 
 
 class TipoBemForm(forms.ModelForm):
     class Meta:
         model = TipoBem
         fields = ["descricao"]
+
+
+class EstadoConservacaoForm(forms.ModelForm):
+    class Meta:
+        model = EstadoConservacao
+        fields = ["nivel", "descricao"]
