874d312c877faa1a00535fefb317af416dd5fe1e
kavvata <gabriel.kawata@gmail.com>
2025-08-11 08:22:35 -0300

    feat(ensino): implementado entity de campus

---
+++ ensino/domain/entities.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+from core.domain.entities import TimeStampableEntity
+
+
+@dataclass(kw_only=True)
+class CampusEntity(TimeStampableEntity):
+    sigla: str
+    nome: str
