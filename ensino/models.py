0426531d46ee4f6c577b7269b8bf6f06122841de
kavvata <gabriel.kawata@gmail.com>
2025-08-14 08:45:30 -0300

    feat(ensino): implementado modelo ORM para Campus

--- ensino/models.py
+++ ensino/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from core.models import Timestampable
+
+
 # Create your models here.
+class Campus(Timestampable, models.Model):
+    sigla = models.CharField(null=False, blank=False, max_length=32)
+    descricao = models.CharField(null=False, blank=False, max_length=255)
