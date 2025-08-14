6611f6f4d183240d9744f2b32f5b8ff616185eac
kavvata <gabriel.kawata@gmail.com>
2025-08-14 08:05:33 -0300

    feat(ensino): implementacao de usecase para cadastro de campus

--- ensino/usecases.py
+++ ensino/usecases.py
@@ -1,4 +1,5 @@
 from core.types import ResultError, ResultSuccess
+from ensino.domain.entities import CampusEntity
 from ensino.policies.contracts import CampusPolicy
 from ensino.repositories.contracts import CampusRepository
 
@@ -20,3 +21,25 @@ class ListarCampiUsecase:
             return ResultSuccess(resposta)
         except Exception as e:
             return ResultError(f"Erro ao listar campi: {e}")
+
+
+class CadastrarCampusUsecase:
+    def __init__(self, repo: CampusRepository, policy: CampusPolicy) -> None:
+        self.repo = repo
+        self.policy = policy
+
+    def pode_criar(self):
+        return self.policy.pode_criar()
+
+    def execute(self, novo_campus: CampusEntity):
+        if not self.policy.pode_criar():
+            return ResultError("Você não tem permissão para cadastrar campus.")
+
+        try:
+            resposta = self.repo.cadastrar_campus(
+                novo_campus,
+                self.policy.user,
+            )
+            return ResultSuccess(resposta)
+        except Exception as e:
+            return ResultError(f"Erro ao cadastrar campus: {e}")
