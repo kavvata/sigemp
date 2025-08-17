2f76f284cc315b9d2cfb8ccac44170fc20a603c6
kavvata <gabriel.kawata@gmail.com>
2025-08-17 18:24:45 -0300

    feat(ensino): criado contratos de policy e repository para cursos

--- ensino/policies/contracts.py
+++ ensino/policies/contracts.py
@@ -25,3 +25,29 @@ class CampusPolicy(ABC):
     @abstractmethod
     def pode_visualizar(self, campus) -> bool:
         pass
+
+
+class CursoPolicy(ABC):
+    def __init__(self, user) -> None:
+        super().__init__()
+        self.user = user
+
+    @abstractmethod
+    def pode_listar(self) -> bool:
+        pass
+
+    @abstractmethod
+    def pode_criar(self) -> bool:
+        pass
+
+    @abstractmethod
+    def pode_editar(self, curso) -> bool:
+        pass
+
+    @abstractmethod
+    def pode_remover(self, curso) -> bool:
+        pass
+
+    @abstractmethod
+    def pode_visualizar(self, curso) -> bool:
+        pass
--- ensino/repositories/contracts.py
+++ ensino/repositories/contracts.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 from typing import Any
 
-from ensino.domain.entities import CampusEntity
+from ensino.domain.entities import CampusEntity, CursoEntity
 
 
 class CampusRepository(ABC):
@@ -24,4 +24,26 @@ class CampusRepository(ABC):
 
     @abstractmethod
     def remover_campus(self, id: int, user: Any):
+        pass
+
+
+class CursoRepository(ABC):
+    @abstractmethod
+    def listar_cursos(self):
+        pass
+
+    @abstractmethod
+    def buscar_por_id(self, id: int):
+        pass
+
+    @abstractmethod
+    def cadastrar_curso(self, curso: CursoEntity, user: Any):
+        pass
+
+    @abstractmethod
+    def editar_curso(self, curso: CursoEntity, user: Any):
+        pass
+
+    @abstractmethod
+    def remover_curso(self, id: int, user: Any):
         pass
