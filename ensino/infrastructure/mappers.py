1e4d296e7e820a6adef5b60a29bda6ae19eeb90e
kavvata <gabriel.kawata@gmail.com>
2025-08-17 19:46:15 -0300

    feat(ensino): implementado mapper para curso

--- ensino/infrastructure/mappers.py
+++ ensino/infrastructure/mappers.py
@@ -1,6 +1,6 @@
 from django.forms import model_to_dict
-from ensino.domain.entities import CampusEntity
-from ensino.models import Campus
+from ensino.domain.entities import CampusEntity, CursoEntity
+from ensino.models import Campus, Curso
 
 
 class CampusMapper:
@@ -11,3 +11,13 @@ class CampusMapper:
     @staticmethod
     def from_dict(data: dict):
         return CampusEntity(**data)
+
+
+class CursoMapper:
+    @staticmethod
+    def from_model(instance: Curso):
+        return CursoEntity(**model_to_dict(instance))
+
+    @staticmethod
+    def from_dict(data: dict):
+        return CursoEntity(**data)
