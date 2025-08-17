e6767df168481ae292600aef41aafe4cf0f32a96
kavvata <gabriel.kawata@gmail.com>
2025-08-17 19:50:54 -0300

    feat(ensino): implementado classe form para cursos

--- ensino/presentation/forms.py
+++ ensino/presentation/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from ensino.models import Campus
+from ensino.models import Campus, Curso
 
 
 class CampusForm(forms.ModelForm):
     class Meta:
         model = Campus
         fields = ["nome", "sigla"]
+
+
+class CursoForm(forms.ModelForm):
+    class Meta:
+        model = Curso
+        fields = ["nome", "sigla", "campus"]
