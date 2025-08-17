9ba8a31c0cb42e09616d1d41c967f9c8ff88a9ac
kavvata <gabriel.kawata@gmail.com>
2025-08-17 19:45:48 -0300

    feat(ensino): implementado classe policy para cursos

--- ensino/policies/django.py
+++ ensino/policies/django.py
@@ -1,6 +1,6 @@
 from typing import override
 from django.contrib.auth.models import User
-from ensino.policies.contracts import CampusPolicy
+from ensino.policies.contracts import CampusPolicy, CursoPolicy
 
 
 class DjangoCampusPolicy(CampusPolicy):
@@ -37,4 +37,41 @@ class DjangoCampusPolicy(CampusPolicy):
             self.user.is_superuser
             or self.user.has_perm("ensino:view_campus")
             and campus.removido_em is None
+        )
+
+
+class DjangoCursoPolicy(CursoPolicy):
+    def __init__(self, user: User) -> None:
+        super().__init__(user)
+
+    @override
+    def pode_listar(self) -> bool:
+        return self.user.is_superuser or self.user.has_perm("ensino:view_curso")
+
+    @override
+    def pode_criar(self) -> bool:
+        return self.user.is_superuser or self.user.has_perm("ensino:add_curso")
+
+    @override
+    def pode_editar(self, curso) -> bool:
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("ensino:change_curso")
+            and curso.removido_em is None
+        )
+
+    @override
+    def pode_remover(self, curso) -> bool:
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("ensino:delete_curso")
+            and curso.removido_em is None
+        )
+
+    @override
+    def pode_visualizar(self, curso) -> bool:
+        return (
+            self.user.is_superuser
+            or self.user.has_perm("ensino:view_curso")
+            and curso.removido_em is None
         )
