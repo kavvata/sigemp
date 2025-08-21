0307f7b76b287efce685aa6b1b9f9ca3659901d4
kavvata <gabriel.kawata@gmail.com>
2025-08-21 09:51:34 -0300

    feat(ensino): implementado entity de forma de selecao

--- ensino/domain/entities.py
+++ ensino/domain/entities.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
+from datetime import datetime
 
 from core.domain.entities import TimeStampableEntity
 
@@ -21,3 +22,11 @@ class CursoEntity(TimeStampableEntity):
 
     def __str__(self) -> str:
         return f"{self.nome} ({self.campus_sigla})"
+
+
+@dataclass(kw_only=True)
+class FormaSelecaoEntity(TimeStampableEntity):
+    descricao: str
+    periodo_inicio: datetime
+    periodo_fim: datetime
+    id: Optional[int] = None
