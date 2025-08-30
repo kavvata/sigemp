db4fa843ca0cd1d29aee7a39f689b3644d613e64
kavvata <gabriel.kawata@gmail.com>
2025-08-30 16:13:16 -0300

    refactor(ensino): criado pacote para usecases no app ensino

---
+++ ensino/usecases/__init__.py
@@ -0,0 +1,34 @@
+from .campus_usecases import (
+    ListarCampiUsecase,
+    CadastrarCampusUsecase,
+    EditarCampusUsecase,
+    RemoverCampusUsecase,
+)
+from .curso_usecases import (
+    ListarCursosUsecase,
+    CadastrarCursoUsecase,
+    EditarCursoUsecase,
+    RemoverCursoUsecase,
+)
+
+from .formaselecao_usecases import (
+    ListarFormasSelecaoUsecase,
+    CadastrarFormaSelecaoUsecase,
+    EditarFormaSelecaoUsecase,
+    RemoverFormaSelecaoUsecase,
+)
+
+__all__ = [
+    "ListarCampiUsecase",
+    "CadastrarCampusUsecase",
+    "EditarCampusUsecase",
+    "RemoverCampusUsecase",
+    "ListarCursosUsecase",
+    "CadastrarCursoUsecase",
+    "EditarCursoUsecase",
+    "ListarFormasSelecaoUsecase",
+    "CadastrarFormaSelecaoUsecase",
+    "EditarFormaSelecaoUsecase",
+    "RemoverFormaSelecaoUsecase",
+    "RemoverCursoUsecase",
+]
