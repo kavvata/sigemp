57a9c3ca65b8d67d80935f6a453ebd6ed16a7d4b
kavvata <gabriel.kawata@gmail.com>
2025-09-10 17:57:47 -0300

    test(emprestimo): criado testes para tipos de ocorrencia

---
+++ tests/emprestimo/test_tipoocorrencia_integracao.py
@@ -0,0 +1,202 @@
+from datetime import datetime, timedelta
+import pytest
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from pytest_django.asserts import (
+    assertContains,
+    assertNotContains,
+    assertTemplateNotUsed,
+    assertTemplateUsed,
+)
+
+from ensino.domain.entities import TipoOcorrenciaEntity
+from ensino.models import TipoOcorrencia
+
+
+@pytest.fixture
+def test_user(db):
+    User = get_user_model()
+    user = User.objects.create_user(username="testuser", password="testpassword")
+    return user
+
+
+@pytest.fixture
+def tipo_ocorrencia_entity():
+    return TipoOcorrenciaEntity(id=1, descricao="Perda")
+
+
+@pytest.fixture
+def tipo_ocorrencia(db):
+    entity = TipoOcorrenciaEntity(id=1, descricao="Perda")
+    model, _criado = TipoOcorrencia.objects.get_or_create(entity.to_dict())
+    yield model
+
+    model.delete()
+
+
+@pytest.fixture
+def lista_tipos_ocorrencia(db):
+    lista_entities = [
+        TipoOcorrenciaEntity(id=1, descricao="Perda"),
+        TipoOcorrenciaEntity(id=2, descricao="Roubo"),
+        TipoOcorrenciaEntity(id=3, descricao="Extravio"),
+        TipoOcorrenciaEntity(id=4, descricao="Dano"),
+    ]
+
+    tipos_ocorrencia_models = [
+        TipoOcorrencia.objects.get_or_create(**e.to_dict())[0] for e in lista_entities
+    ]
+    yield tipos_ocorrencia_models
+
+    TipoOcorrencia.objects.filter(
+        descricao__in=[e.descricao for e in lista_entities]
+    ).delete()
+
+
+@pytest.mark.django_db
+def test_listar_tipos_ocorrencia(lista_tipos_ocorrencia, admin_client):
+    response = admin_client.get(reverse_lazy("ensino:listar_tipos_ocorrencia"))
+    for descricao in [
+        tipo_ocorrencia.descricao for tipo_ocorrencia in lista_tipos_ocorrencia
+    ]:
+        assertContains(response, descricao)
+
+    assertTemplateUsed(response, "ensino/tipo_ocorrencia/tiposocorrencia_list.html")
+
+
+@pytest.mark.django_db
+def test_nao_pode_listar_tipos_ocorrencia(client, test_user):
+    client.force_login(test_user)
+
+    url = reverse_lazy("ensino:listar_tipos_ocorrencia")
+    response = client.get(url)
+
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_criar_tipo_ocorrencia(
+    admin_client, tipo_ocorrencia_entity: TipoOcorrenciaEntity
+):
+    url = reverse_lazy("ensino:criar_tipo_ocorrencia")
+    response = admin_client.get(url)
+    assertTemplateUsed(response, "ensino/tipo_ocorrencia/tiposocorrencia_form.html")
+
+    assert response.status_code == 200
+
+    form_data = tipo_ocorrencia_entity.to_dict(["timestamps", "id"])
+    form_data["periodo_inicio"] = form_data["periodo_inicio"].strftime("%Y-%m-%d")
+    form_data["periodo_fim"] = form_data["periodo_fim"].strftime("%Y-%m-%d")
+
+    response = admin_client.post(url, form_data, follow=True)
+
+    assert response.status_code == 200
+    assert TipoOcorrencia.objects.filter(
+        descricao=tipo_ocorrencia_entity.descricao
+    ).exists()
+    assertContains(response, tipo_ocorrencia_entity.descricao)
+    assertTemplateUsed(response, "ensino/tipo_ocorrencia/tiposocorrencia_list.html")
+
+
+@pytest.mark.django_db
+def test_nao_pode_criar_tipo_ocorrencia(
+    client, test_user, tipo_ocorrencia_entity: TipoOcorrenciaEntity
+):
+    client.force_login(test_user)
+
+    url = reverse_lazy("ensino:criar_tipo_ocorrencia")
+
+    response = client.get(url)
+
+    assert response.status_code == 403
+
+    form_data = tipo_ocorrencia_entity.to_dict(["timestamps", "id"])
+    form_data["periodo_inicio"] = form_data["periodo_inicio"].strftime("%Y-%m-%d")
+    form_data["periodo_fim"] = form_data["periodo_fim"].strftime("%Y-%m-%d")
+
+    response = client.post(url, form_data, follow=True)
+
+    assert response.status_code == 403
+    assert not TipoOcorrencia.objects.filter(
+        descricao=tipo_ocorrencia_entity.descricao
+    ).exists()
+    assertTemplateNotUsed(response, "ensino/tipo_ocorrencia/tiposocorrencia_form.html")
+
+
+@pytest.mark.django_db
+def test_editar_tipo_ocorrencia(
+    admin_client, tipo_ocorrencia_entity: TipoOcorrenciaEntity, tipo_ocorrencia
+):
+    url = reverse_lazy("ensino:editar_tipo_ocorrencia", args=[tipo_ocorrencia.pk])
+
+    response = admin_client.get(url)
+
+    assertTemplateUsed(response, "ensino/tipo_ocorrencia/tiposocorrencia_form.html")
+    assert response.status_code == 200
+
+    original = tipo_ocorrencia.descricao
+    tipo_ocorrencia_entity.id = tipo_ocorrencia.id
+    tipo_ocorrencia_entity.descricao = "Edital Número 01/2020"
+
+    form_data = tipo_ocorrencia_entity.to_dict(exclude=["timestamps"])
+    form_data["periodo_inicio"] = form_data["periodo_inicio"].strftime("%Y-%m-%d")
+    form_data["periodo_fim"] = form_data["periodo_fim"].strftime("%Y-%m-%d")
+
+    response = admin_client.post(url, form_data, follow=True)
+    tipo_ocorrencia.refresh_from_db()
+    assert tipo_ocorrencia.descricao == tipo_ocorrencia_entity.descricao
+    assertContains(response, tipo_ocorrencia_entity.descricao)
+    assertTemplateUsed(response, "ensino/tipo_ocorrencia/tiposocorrencia_list.html")
+
+
+@pytest.mark.django_db
+def test_nao_pode_editar_tipo_ocorrencia(
+    client, test_user, tipo_ocorrencia_entity: TipoOcorrenciaEntity, tipo_ocorrencia
+):
+    client.force_login(test_user)
+    url = reverse_lazy("ensino:editar_tipo_ocorrencia", args=[tipo_ocorrencia.pk])
+
+    response = client.get(url)
+
+    assert response.status_code == 403
+
+    original = tipo_ocorrencia.descricao
+
+    tipo_ocorrencia_entity.id = tipo_ocorrencia.id
+    tipo_ocorrencia_entity.descricao = "Edital Nº XXX/2025"
+
+    form_data = tipo_ocorrencia_entity.to_dict(exclude=["timestamps"])
+    form_data["periodo_inicio"] = form_data["periodo_inicio"].strftime("%Y-%m-%d")
+    form_data["periodo_fim"] = form_data["periodo_fim"].strftime("%Y-%m-%d")
+
+    response = client.post(url, form_data, follow=True)
+    assertTemplateNotUsed(response, "ensino/tipo_ocorrencia/tiposocorrencia_list.html")
+    tipo_ocorrencia.refresh_from_db()
+    assert tipo_ocorrencia.descricao == original
+
+
+@pytest.mark.django_db
+def test_remover_tipo_ocorrencia(admin_client, tipo_ocorrencia):
+    url = reverse_lazy("ensino:remover_tipo_ocorrencia", args=[tipo_ocorrencia.pk])
+
+    response = admin_client.post(url, follow=True)
+
+    tipo_ocorrencia.refresh_from_db()
+
+    assert response.status_code == 200
+    assert tipo_ocorrencia.removido_em is not None
+
+    response = admin_client.get(reverse_lazy("ensino:listar_tipos_ocorrencia"))
+    assertNotContains(response, tipo_ocorrencia.descricao)
+
+
+@pytest.mark.django_db
+def test_nao_pode_remover_tipo_ocorrencia(client, test_user, tipo_ocorrencia):
+    client.force_login(test_user)
+
+    url = reverse_lazy("ensino:remover_tipo_ocorrencia", args=[tipo_ocorrencia.pk])
+    response = client.post(url, follow=True)
+
+    tipo_ocorrencia.refresh_from_db()
+    assert tipo_ocorrencia.removido_em is None
+    assert response.status_code == 403
---
+++ tests/emprestimo/test_tipoocorrencia_usecases.py
@@ -0,0 +1,199 @@
+from unittest import mock
+import pytest
+
+from core.types import ResultError, ResultSuccess
+from emprestimo.domain.entities import TipoOcorrenciaEntity
+from emprestimo.usecases import (
+    ListarTiposOcorrenciaUsecase,
+    CadastrarTipoOcorrenciaUsecase,
+    EditarTipoOcorrenciaUsecase,
+    RemoverTipoOcorrenciaUsecase,
+)
+
+
+@pytest.fixture
+def tipo_ocorrencia():
+    return TipoOcorrenciaEntity(id=1, descricao="Perda")
+
+
+@pytest.fixture
+def lista_tipos_ocorrencia():
+    return [
+        TipoOcorrenciaEntity(id=1, descricao="Perda"),
+        TipoOcorrenciaEntity(id=2, descricao="Roubo"),
+        TipoOcorrenciaEntity(id=3, descricao="Extravio"),
+        TipoOcorrenciaEntity(id=4, descricao="Dano"),
+    ]
+
+
+def test_listar_tipos_ocorrencia(lista_tipos_ocorrencia):
+    repo = mock.Mock()
+    policy = mock.Mock()
+
+    repo.listar_tipos_ocorrencia.return_value = lista_tipos_ocorrencia
+    policy.pode_listar.return_value = True
+
+    usecase = ListarTiposOcorrenciaUsecase(repo, policy)
+
+    if usecase.pode_listar():
+        result = usecase.execute()
+
+    repo.listar_tipos_ocorrencia.assert_called_with()
+    policy.pode_listar.assert_called_with()
+
+    assert result
+    assert isinstance(result, ResultSuccess)
+    assert result.value == lista_tipos_ocorrencia
+
+
+def test_nao_pode_listar_tipos_ocorrencia_usecase(lista_tipos_ocorrencia):
+    repo = mock.Mock()
+    policy = mock.Mock()
+
+    repo.listar_tipos_ocorrencia.return_value = lista_tipos_ocorrencia
+    policy.pode_listar.return_value = False
+
+    usecase = ListarTiposOcorrenciaUsecase(repo, policy)
+
+    result = usecase.execute()
+
+    repo.listar_tipos_ocorrencia.assert_not_called()
+    policy.pode_listar.assert_called_with()
+
+    assert not result
+    assert isinstance(result, ResultError)
+
+
+def test_cadastrar_tipo_ocorrencia_usecase(tipo_ocorrencia):
+    repo = mock.Mock()
+    policy = mock.Mock()
+    user = mock.Mock()
+    repo.cadastrar_tipo_ocorrencia.return_value = tipo_ocorrencia
+    policy.pode_criar.return_value = True
+    policy.user = user
+
+    usecase = CadastrarTipoOcorrenciaUsecase(repo, policy)
+
+    if usecase.pode_criar():
+        result = usecase.execute(tipo_ocorrencia.descricao)
+
+    repo.cadastrar_tipo_ocorrencia.assert_called_with(tipo_ocorrencia.descricao, user)
+    policy.pode_criar.assert_called_with()
+
+    assert result
+    assert isinstance(result, ResultSuccess)
+    assert result.value == tipo_ocorrencia
+
+
+def test_nao_pode_cadastrar_tipo_ocorrencia_usecase(tipo_ocorrencia):
+    repo = mock.Mock()
+    policy = mock.Mock()
+    user = mock.Mock()
+    repo.cadastrar_tipo_ocorrencia.return_value = tipo_ocorrencia
+    policy.pode_criar.return_value = False
+    policy.user = user
+
+    usecase = CadastrarTipoOcorrenciaUsecase(repo, policy)
+
+    result = usecase.execute(tipo_ocorrencia.descricao)
+
+    repo.cadastrar_tipo_ocorrencia.assert_not_called()
+    policy.pode_criar.assert_called_with()
+
+    assert not result
+    assert isinstance(result, ResultError)
+
+
+def test_editar_tipo_ocorrencia_usecase(tipo_ocorrencia):
+    repo = mock.Mock()
+    policy = mock.Mock()
+    user = mock.Mock()
+    repo.buscar_por_id.return_value = tipo_ocorrencia
+    repo.editar_tipo_ocorrencia.return_value = tipo_ocorrencia
+    policy.pode_editar.return_value = True
+    policy.user = user
+
+    usecase = EditarTipoOcorrenciaUsecase(repo, policy)
+
+    result = usecase.get_tipo_ocorrencia(tipo_ocorrencia.id)
+    encontrado = result.value
+
+    assert encontrado
+
+    result = usecase.execute(encontrado.id, encontrado.descricao)
+
+    repo.buscar_por_id.assert_called_with(tipo_ocorrencia.id)
+    repo.editar_tipo_ocorrencia.assert_called_with(
+        tipo_ocorrencia.id, tipo_ocorrencia.descricao, user
+    )
+    policy.pode_editar.assert_called_with(tipo_ocorrencia)
+
+    assert result
+    assert isinstance(result, ResultSuccess)
+    assert result.value == tipo_ocorrencia
+
+
+def test_nao_pode_editar_tipo_ocorrencia_usecase(tipo_ocorrencia):
+    repo = mock.Mock()
+    policy = mock.Mock()
+    user = mock.Mock()
+    repo.buscar_por_id.return_value = tipo_ocorrencia
+    repo.editar_tipo_ocorrencia.return_value = tipo_ocorrencia
+    policy.pode_editar.return_value = False
+    policy.user = user
+
+    usecase = EditarTipoOcorrenciaUsecase(repo, policy)
+
+    result = usecase.get_tipo_ocorrencia(tipo_ocorrencia.id)
+    assert not result
+    assert isinstance(result, ResultError)
+
+    result = usecase.execute(tipo_ocorrencia.id, tipo_ocorrencia.descricao)
+
+    repo.buscar_por_id.assert_called_with(tipo_ocorrencia.id)
+    repo.editar_tipo_ocorrencia.assert_not_called()
+    policy.pode_editar.assert_called_with(tipo_ocorrencia)
+
+    assert not result
+    assert isinstance(result, ResultError)
+
+
+def test_remover_tipo_ocorrencia_usecase(tipo_ocorrencia):
+    repo = mock.Mock()
+    policy = mock.Mock()
+    user = mock.Mock()
+    repo.buscar_por_id.return_value = tipo_ocorrencia
+    repo.remover_tipo_ocorrencia.return_value = tipo_ocorrencia
+    policy.pode_remover.return_value = True
+    policy.user = user
+
+    usecase = RemoverTipoOcorrenciaUsecase(repo, policy)
+
+    result = usecase.execute(tipo_ocorrencia.id)
+
+    repo.remover_tipo_ocorrencia.assert_called_with(tipo_ocorrencia.id, user)
+    policy.pode_remover.assert_called_with(tipo_ocorrencia)
+
+    assert result
+    assert isinstance(result, ResultSuccess)
+    assert result.value == tipo_ocorrencia
+
+
+def test_nao_pode_remover_tipo_ocorrencia_usecase(tipo_ocorrencia):
+    repo = mock.Mock()
+    policy = mock.Mock()
+    user = mock.Mock()
+    repo.buscar_por_id.return_value = tipo_ocorrencia
+    repo.remover_tipo_ocorrencia.return_value = tipo_ocorrencia
+    policy.pode_remover.return_value = False
+    policy.user = user
+
+    usecase = RemoverTipoOcorrenciaUsecase(repo, policy)
+
+    result = usecase.execute(tipo_ocorrencia.id)
+
+    repo.remover_tipo_ocorrencia.assert_not_called()
+    policy.pode_remover.assert_called_with(tipo_ocorrencia)
+
+    assert not result
+    assert isinstance(result, ResultError)
