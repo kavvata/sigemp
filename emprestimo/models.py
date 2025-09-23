ae36872f95647b47dee362d0164e95648f848138
kavvata <gabriel.kawata@gmail.com>
2025-09-23 08:35:29 -0300

    feat(emprestimo): implementacao de modelo ORM para tipos de ocorrencia

--- emprestimo/models.py
+++ emprestimo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from core.models import Timestampable
+
+
 # Create your models here.
+class TipoOcorrencia(Timestampable, models.Model):
+    descricao = models.CharField(null=False, blank=False, max_length=255)
