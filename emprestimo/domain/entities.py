fcb27366fe41eba6af93593bedea82c5dd1c4302
kavvata <gabriel.kawata@gmail.com>
2025-09-25 12:03:44 -0300

    feat(emprestimo): implementado entidade de emprestimo

--- emprestimo/domain/entities.py
+++ emprestimo/domain/entities.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from datetime import date
+from typing import Optional
 
 from core.domain.entities import TimeStampableEntity
 
@@ -7,3 +9,15 @@ from core.domain.entities import TimeStampableEntity
 class TipoOcorrenciaEntity(TimeStampableEntity):
     descricao: str
     id: int = None
+
+
+@dataclass(kw_only=True)
+class EmprestimoEntity(TimeStampableEntity):
+    data_emprestimo: date
+    data_devolucao_prevista: date
+    data_devolucao: Optional[date]
+    devolucao_ciente_por_id: Optional[int] = None
+    bem_id: Optional[int] = None
+    aluno_id: Optional[int] = None
+    estado: int
+    id: Optional[int] = None
