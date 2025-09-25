88924545964cbd2bfd2a6ef317ba8d0daf6d8b7b
kavvata <gabriel.kawata@gmail.com>
2025-09-25 09:56:46 -0300

    feat(emprestimo): implementado to str de tipo de ocorrencia

--- emprestimo/models.py
+++ emprestimo/models.py
@@ -6,3 +6,6 @@ from core.models import Timestampable
 # Create your models here.
 class TipoOcorrencia(Timestampable, models.Model):
     descricao = models.CharField(null=False, blank=False, max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.descricao}"
