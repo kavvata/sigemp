72198b16395a539931aca222a253c94015d277a7
kavvata <gabriel.kawata@gmail.com>
2025-09-27 13:07:23 -0300

    feat(emprestimo): criado contratos de repositorio e policy para emprestimos

--- emprestimo/policies/contracts.py
+++ emprestimo/policies/contracts.py
@@ -26,3 +26,29 @@ class TipoOcorrenciaPolicy(ABC):
     @abstractmethod
     def pode_visualizar(self, campus) -> bool:
         pass
+
+
+class EmprestimoPolicy(ABC):
+    def __init__(self, user) -> None:
+        super().__init__()
+        self.user = user
+
+    @abstractmethod
+    def pode_listar(self) -> bool:
+        pass
+
+    @abstractmethod
+    def pode_criar(self) -> bool:
+        pass
+
+    @abstractmethod
+    def pode_editar(self, campus) -> bool:
+        pass
+
+    @abstractmethod
+    def pode_remover(self, campus) -> bool:
+        pass
+
+    @abstractmethod
+    def pode_visualizar(self, campus) -> bool:
+        pass
--- emprestimo/repositories/contracts.py
+++ emprestimo/repositories/contracts.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
-from emprestimo.domain.entities import TipoOcorrenciaEntity
+from emprestimo.domain.entities import TipoOcorrenciaEntity, EmprestimoEntity
 
 
 class TipoOcorrenciaRepository(ABC):
@@ -25,4 +25,34 @@ class TipoOcorrenciaRepository(ABC):
 
     @abstractmethod
     def remover_tipo_ocorrencia(self, id: int, user: Any):
+        pass
+
+
+class EmprestimoRepository(ABC):
+    @abstractmethod
+    def listar_emprestimos(self):
+        pass
+
+    @abstractmethod
+    def buscar_por_id(self, id: int):
+        pass
+
+    @abstractmethod
+    def cadastrar_emprestimo(self, emprestimo: EmprestimoEntity, user: Any):
+        pass
+
+    @abstractmethod
+    def editar_emprestimo(self, emprestimo: EmprestimoEntity, user: Any):
+        pass
+
+    @abstractmethod
+    def remover_emprestimo(self, id: int, user: Any):
+        pass
+
+    @abstractmethod
+    def gerar_termo_responsabilidade(self, emprestimo: EmprestimoEntity, user: Any):
+        pass
+
+    @abstractmethod
+    def gerar_termo_devolucao(self, emprestimo: EmprestimoEntity, user: Any):
         pass
