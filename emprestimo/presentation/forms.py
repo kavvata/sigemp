6df0017f230fc744cdfb21a5e4b28acc456b21f6
kavvata <gabriel.kawata@gmail.com>
2025-09-27 20:31:45 -0300

    feat(emprestimo): criado classe formulario para cadastro de emprestimo

--- emprestimo/presentation/forms.py
+++ emprestimo/presentation/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 
-from emprestimo.models import TipoOcorrencia
+from emprestimo.models import Emprestimo, TipoOcorrencia
 
 
 class TipoOcorrenciaForm(forms.ModelForm):
     class Meta:
         model = TipoOcorrencia
         fields = ["descricao"]
+
+
+class CriarEmprestimoForm(forms.ModelForm):
+    class Meta:
+        model = Emprestimo
+        fields = [
+            "aluno",
+            "bem",
+            "observacoes",
+            "data_emprestimo",
+            "data_devolucao_prevista",
+        ]
