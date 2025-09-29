8ed5064f444f302531f38ee09006dbfa56776d68
kavvata <gabriel.kawata@gmail.com>
2025-09-28 21:28:57 -0300

    feat(emprestimo): alterado retorno para response gerada por django-weasyprint

--- emprestimo/infrastructure/services/django.py
+++ emprestimo/infrastructure/services/django.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from emprestimo.domain.entities import EmprestimoEntity
 from emprestimo.infrastructure.services.contracts import PDFService
 
@@ -8,22 +9,24 @@ from io import BytesIO
 
 
 class DjangoWeasyPDFService(PDFService):
+    def __init__(self, request) -> None:
+        self._request = request
+
     def gerar_pdf_de_template(self, template_path: str, context: dict) -> BytesIO:
         html_string = render_to_string(template_path, context)
 
         response = WeasyTemplateResponse(
-            request=None,
+            request=self._request,
             template=template_path,
             context=context,
             content_type="application/pdf",
         )
 
-        pdf_buffer = BytesIO()
-        response.render_to_response(pdf_buffer)
-        pdf_buffer.seek(0)
-        return pdf_buffer
+        return response
 
-    def gerar_termo_responsabilidade(self, emprestimo: EmprestimoEntity) -> BytesIO:
+    def gerar_termo_responsabilidade(
+        self, emprestimo: EmprestimoEntity, user: User
+    ) -> BytesIO:
         from emprestimo.models import Emprestimo
 
         model = Emprestimo.objects.get(id=emprestimo.id)
@@ -33,11 +36,13 @@ class DjangoWeasyPDFService(PDFService):
             "data_geracao": timezone.now().strftime("%d/%m/%Y às %H:%M"),
         }
 
-        return self.generate_pdf_from_template(
+        return self.gerar_pdf_de_template(
             "emprestimo/emprestimo/termo_responsabilidade.html", context
         )
 
-    def gerar_termo_devolucao(self, emprestimo: EmprestimoEntity) -> BytesIO:
+    def gerar_termo_devolucao(
+        self, emprestimo: EmprestimoEntity, user: User
+    ) -> BytesIO:
         from emprestimo.models import Emprestimo
 
         model = Emprestimo.objects.get(id=emprestimo.id)
@@ -47,6 +52,6 @@ class DjangoWeasyPDFService(PDFService):
             "data_geracao": timezone.now().strftime("%d/%m/%Y às %H:%M"),
         }
 
-        return self.generate_pdf_from_template(
+        return self.gerar_pdf_de_template(
             "emprestimo/emprestimo/termo_devolucao.html", context
         )
