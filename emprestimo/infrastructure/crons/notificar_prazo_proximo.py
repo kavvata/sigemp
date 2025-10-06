a477d8f407a960020c65836c28833f2d552dd272
kavvata <gabriel.kawata@gmail.com>
2025-10-06 19:12:59 -0300

    feat(cron): implementado cron para notificacao de prazos proximos

---
+++ emprestimo/infrastructure/crons/notificar_prazo_proximo.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+from django.utils import timezone
+from emprestimo.infrastructure.services.mail_django import DjangoMailService
+from emprestimo.repositories.django import DjangoEmprestimoRepository
+from emprestimo.usecases.emprestimo_usecases import NotificarDevolucaoUsecase
+from ensino.repositories.django import DjangoAlunoRepository
+
+
+def cron_notificar_prazo_proximo():
+    repo = DjangoEmprestimoRepository()
+    aluno_repo = DjangoAlunoRepository()
+    service = DjangoMailService()
+
+    usecase = NotificarDevolucaoUsecase(
+        repo,
+        aluno_repo,
+        service,
+    )
+
+    resultado = usecase.execute(
+        timezone.now() + timedelta(days=7),
+    )
+
+    if not resultado:
+        print(f"[CRON] Erro ao notificar prazos: {resultado.mensagem}")
+    else:
+        print(f"[CRON] Notificações de devolução enviadas: {resultado.mensagem}")
